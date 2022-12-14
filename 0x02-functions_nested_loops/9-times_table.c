cfb18f8f6ebb6b11a5d87a3c8e88a5c85ba4ade5
destinygerald <destinygerald85@gmail.com>
2022-12-14 03:40:34 +0100

    task done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,10 @@ void times_table(void)
 {
 	int num, mul, prod;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 1; num <= 9; num++)
 	{
-		for (mul = 0; mul <= 9; mul++)
+		_putchar('0')
+		for (mul = 1; mul <= 9; mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
